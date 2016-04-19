39cf13bde6884478b6163451418948ebf32e0503
Mohamad Jahani <m4m4lj@gmail.com>
2016-04-19 08:28:40 +0430

    feat: add responseHandler

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const DEFAULT_CONFIG = {
     message: 'message'
   },
   validator: msg => true, // eslint-disable-line
-  modifier: msg => msg
+  modifier: msg => msg,
+  responseHandler: msg => null // eslint-disable-line
 };
 
 export default bot => {
@@ -30,7 +31,7 @@ export default bot => {
     const params = type === 'get' ? req.query : req.body;
     // gather from and message data from params passed in request
     const { from: fromKey, message: messageKey } = config.params;
-    const { modifier = null, validator = null } = config;
+    const { modifier = null, validator = null, responseHandler = null } = config;
 
     // Temporary message object may change in modifier call
     let tempMessageObject = {
@@ -53,18 +54,18 @@ export default bot => {
     const { from, message } = tempMessageObject;
 
     const slackUser = bot.users.find(user =>
-      // skip if user has no phone number in slack
+      // Skip if user has no phone number in slack
       user.profile.phone && user.profile.phone.replace(/\+98/, '0') === from
     );
 
-    // if there is no user with that phone in our slack it's 404
+    // If there is no user with that phone in our slack it's 404
     if (!slackUser) {
       return res.sendStatus(404);
     }
 
     const { id } = slackUser;
 
-    // find or create user im with bot
+    // Find or create user im with bot
     let im = (bot.ims.find(i => i.user === id) || {}).id;
     if (!im) {
       im = await bot.call('im.open', { user: id });
@@ -77,7 +78,36 @@ export default bot => {
       user: id,
       channel: im
     };
+
+    if (responseHandler) {
+      // Do something with bot response
+      const middleware = bot.modifiers.preprocess('sendMessage', (channel, text, props = {}) => {
+        // Ignore if it's not our user
+        if (channel !== event.channel) {
+          bot.modifiers.remove('sendMessage', middleware);
+          return [channel, text, props];
+        }
+
+        // It's our user! we should handle callback
+        const responseData = {
+          text,
+          phone: from,
+          user: event.id,
+          channel: event.im
+        };
+
+        // Call handler with responseData
+        responseHandler(responseData);
+
+        // Remove the modifier to prevent handling all the requests after this one!
+        bot.modifiers.remove('sendMessage', middleware);
+
+        return [channel, text, props];
+      });
+    }
+
     bot.inject('message', event);
+
     res.status(200).send(event);
 
     return next();
